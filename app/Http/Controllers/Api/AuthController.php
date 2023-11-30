66b55b2cd2ad50d6dea5c1cd19218fe34471fa5c
SinduAditya <nduujanadi51@gmail.com>
2023-11-30 22:48:24 +0700

    scantum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+   public function register(Request $request){
+        $validatedData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        //membuat user
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'role' => 'user'
+        ]);
+
+        //membuat token
+        $token = $user->createToken('auth_token')->plainTextToken;
+        //return
+        return response()->json([
+            'access_token' => $token,
+            'user' => UserResource::make($user),
+        ]);
+   }
+}
---
+++ app/Http/Controllers/SoalController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SoalRequest;
+use App\Models\Soal;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SoalController extends Controller
+{
+    public function index(Request $request){
+        // $soals = \App\Models\Soal::latest()->paginate(10);
+        $soals = DB::table('soals')
+        ->when($request->input('pertanyaan'), function ($query, $name) {
+            return $query->where('pertanyaan', 'like', '%' . $name . '%');
+        })
+        ->orderBy('id', 'desc')
+        ->paginate(10);
+        return view('pages.soals.index', compact('soals'));
+    }
+
+    public function create(){
+        return view('pages.soals.create');
+    }
+
+    public function store(SoalRequest $request){
+        $data = $request->all();
+        \App\Models\Soal::create($data);
+        return redirect()->route('soal.index')->with('success','Soal successfully created');
+    }
+}
+
